fc218d8bf287d68236713733c6af6ea80a3b318a
tonybuii2003 <longphibui1993@gmail.com>
2023-10-29 16:29:21 -0400

    update main

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "sfmm.h"
 
-int main(int argc, char const *argv[])
-{
-    double *ptr = sf_malloc(sizeof(double));
+int main(int argc, char const *argv[]) {
+    double* ptr = sf_malloc(sizeof(double));
 
     *ptr = 320320320e-320;
 
