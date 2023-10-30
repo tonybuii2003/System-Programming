52a431669086adc0a52e96ec00f7f1be137d3cb4
tonybuii2003 <longphibui1993@gmail.com>
2023-10-29 22:41:58 -0400

    update test

--- hw3/tests/sfmm_tests.c
+++ hw3/tests/sfmm_tests.c
@@ -92,119 +92,192 @@ Test(sfmm_basecode_suite, malloc_too_large, .timeout = TEST_TIMEOUT)
 	cr_assert(sf_errno == ENOMEM, "sf_errno is not ENOMEM!");
 }
 
-// Test(sfmm_basecode_suite, free_no_coalesce, .timeout = TEST_TIMEOUT) {
-// 	sf_errno = 0;
-// 	/* void *x = */ sf_malloc(8);
-// 	void *y = sf_malloc(200);
-// 	/* void *z = */ sf_malloc(1);
+Test(sfmm_basecode_suite, free_no_coalesce, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	/* void *x = */ sf_malloc(8);
+	void *y = sf_malloc(200);
+	/* void *z = */ sf_malloc(1);
 
-// 	sf_free(y);
+	sf_free(y);
 
-// 	assert_free_block_count(0, 2);
-// 	assert_free_block_count(224, 1);
-// 	assert_free_block_count(3760, 1);
-// 	cr_assert(sf_errno == 0, "sf_errno is not zero!");
-// }
+	assert_free_block_count(0, 2);
+	assert_free_block_count(224, 1);
+	assert_free_block_count(3760, 1);
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+}
 
-// Test(sfmm_basecode_suite, free_coalesce, .timeout = TEST_TIMEOUT) {
-// 	sf_errno = 0;
-// 	/* void *w = */ sf_malloc(8);
-// 	void *x = sf_malloc(200);
-// 	void *y = sf_malloc(300);
-// 	/* void *z = */ sf_malloc(4);
+Test(sfmm_basecode_suite, free_coalesce, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	/* void *w = */ sf_malloc(8);
+	void *x = sf_malloc(200);
+	void *y = sf_malloc(300);
+	/* void *z = */ sf_malloc(4);
 
-// 	sf_free(y);
-// 	sf_free(x);
+	sf_free(y);
+	sf_free(x);
 
-// 	assert_free_block_count(0, 2);
-// 	assert_free_block_count(544, 1);
-// 	assert_free_block_count(3440, 1);
-// 	cr_assert(sf_errno == 0, "sf_errno is not zero!");
-// }
+	assert_free_block_count(0, 2);
+	assert_free_block_count(544, 1);
+	assert_free_block_count(3440, 1);
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+}
 
-// Test(sfmm_basecode_suite, freelist, .timeout = TEST_TIMEOUT) {
-// 	void *u = sf_malloc(200);
-// 	/* void *v = */ sf_malloc(300);
-// 	void *w = sf_malloc(200);
-// 	/* void *x = */ sf_malloc(500);
-// 	void *y = sf_malloc(200);
-// 	/* void *z = */ sf_malloc(700);
+Test(sfmm_basecode_suite, freelist, .timeout = TEST_TIMEOUT)
+{
+	void *u = sf_malloc(200);
+	/* void *v = */ sf_malloc(300);
+	void *w = sf_malloc(200);
+	/* void *x = */ sf_malloc(500);
+	void *y = sf_malloc(200);
+	/* void *z = */ sf_malloc(700);
 
-// 	sf_free(u);
-// 	sf_free(w);
-// 	sf_free(y);
+	sf_free(u);
+	sf_free(w);
+	sf_free(y);
 
-// 	assert_free_block_count(0, 4);
-// 	assert_free_block_count(224, 3);
-// 	assert_free_block_count(1808, 1);
-// 	assert_free_list_size(4, 3);
-// 	assert_free_list_size(9, 1);
+	assert_free_block_count(0, 4);
+	assert_free_block_count(224, 3);
+	assert_free_block_count(1808, 1);
+	assert_free_list_size(4, 3);
+	assert_free_list_size(9, 1);
 
-// 	// First block in list should be the most recently freed block.
-// 	int i = 4;
-// 	sf_block *bp = sf_free_list_heads[i].body.links.next;
-// 	cr_assert_eq(bp, (char *)y - 2*sizeof(sf_header),
-// 		     "Wrong first block in free list %d: (found=%p, exp=%p)!",
-//                      i, bp, (char *)y - 2*sizeof(sf_header));
-// }
+	// First block in list should be the most recently freed block.
+	int i = 4;
+	sf_block *bp = sf_free_list_heads[i].body.links.next;
+	cr_assert_eq(bp, (char *)y - 2 * sizeof(sf_header),
+				 "Wrong first block in free list %d: (found=%p, exp=%p)!",
+				 i, bp, (char *)y - 2 * sizeof(sf_header));
+}
 
-// Test(sfmm_basecode_suite, realloc_larger_block, .timeout = TEST_TIMEOUT) {
-// 	void *x = sf_malloc(sizeof(int));
-// 	/* void *y = */ sf_malloc(10);
-// 	x = sf_realloc(x, sizeof(int) * 20);
+Test(sfmm_basecode_suite, realloc_larger_block, .timeout = TEST_TIMEOUT)
+{
+	void *x = sf_malloc(sizeof(int));
+	/* void *y = */ sf_malloc(10);
+	x = sf_realloc(x, sizeof(int) * 20);
 
-// 	cr_assert_not_null(x, "x is NULL!");
-// 	sf_block *bp = (sf_block *)((char *)x - 2*sizeof(sf_header));
-// 	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
-// 	cr_assert((bp->header & ~0xf & 0xffff) == 96,
-// 		  "Realloc'ed block size not what was expected (found=%lu, exp=%lu)!",
-// 		  bp->header & ~0xf, 96);
+	cr_assert_not_null(x, "x is NULL!");
+	sf_block *bp = (sf_block *)((char *)x - 2 * sizeof(sf_header));
+	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
+	cr_assert((bp->header & ~0xf & 0xffff) == 96,
+			  "Realloc'ed block size not what was expected (found=%lu, exp=%lu)!",
+			  bp->header & ~0xf, 96);
 
-// 	assert_free_block_count(0, 2);
-// 	assert_free_block_count(32, 1);
-// 	assert_free_block_count(3888, 1);
-// }
+	assert_free_block_count(0, 2);
+	assert_free_block_count(32, 1);
+	assert_free_block_count(3888, 1);
+}
 
-// Test(sfmm_basecode_suite, realloc_smaller_block_splinter, .timeout = TEST_TIMEOUT) {
-// 	void *x = sf_malloc(sizeof(int) * 20);
-// 	void *y = sf_realloc(x, sizeof(int) * 16);
+Test(sfmm_basecode_suite, realloc_smaller_block_splinter, .timeout = TEST_TIMEOUT)
+{
+	void *x = sf_malloc(sizeof(int) * 20);
+	void *y = sf_realloc(x, sizeof(int) * 16);
 
-// 	cr_assert_not_null(y, "y is NULL!");
-// 	cr_assert(x == y, "Payload addresses are different!");
+	cr_assert_not_null(y, "y is NULL!");
+	cr_assert(x == y, "Payload addresses are different!");
 
-// 	sf_block *bp = (sf_block *)((char*)y - 2*sizeof(sf_header));
-// 	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
-// 	cr_assert((bp->header & ~0xf & 0xffff) == 96,
-// 		  "Block size not what was expected (found=%lu, exp=%lu)!",
-// 		  bp->header & ~0xf & 0xffff, 96);
+	sf_block *bp = (sf_block *)((char *)y - 2 * sizeof(sf_header));
+	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
+	cr_assert((bp->header & ~0xf & 0xffff) == 96,
+			  "Block size not what was expected (found=%lu, exp=%lu)!",
+			  bp->header & ~0xf & 0xffff, 96);
 
-// 	// There should be only one free block of size 3952.
-// 	assert_free_block_count(0, 1);
-// 	assert_free_block_count(3952, 1);
-// }
+	// There should be only one free block of size 3952.
+	assert_free_block_count(0, 1);
+	assert_free_block_count(3952, 1);
+}
 
-// Test(sfmm_basecode_suite, realloc_smaller_block_free_block, .timeout = TEST_TIMEOUT) {
-// 	void *x = sf_malloc(sizeof(double) * 8);
-// 	void *y = sf_realloc(x, sizeof(int));
+Test(sfmm_basecode_suite, realloc_smaller_block_free_block, .timeout = TEST_TIMEOUT)
+{
+	printf("begin \n");
+	void *x = sf_malloc(sizeof(double) * 8);
+	printf("Got here \n");
+	void *y = sf_realloc(x, sizeof(int));
 
-// 	cr_assert_not_null(y, "y is NULL!");
+	cr_assert_not_null(y, "y is NULL!");
 
-// 	sf_block *bp = (sf_block *)((char*)y - 2*sizeof(sf_header));
-// 	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
-// 	cr_assert((bp->header & ~0xf & 0xffff) == 32,
-// 		  "Realloc'ed block size not what was expected (found=%lu, exp=%lu)!",
-// 		  bp->header & ~0xf & 0xffff, 32);
+	sf_block *bp = (sf_block *)((char *)y - 2 * sizeof(sf_header));
+	cr_assert(bp->header & 0x8, "Allocated bit is not set!");
+	cr_assert((bp->header & ~0xf & 0xffff) == 32,
+			  "Realloc'ed block size not what was expected (found=%lu, exp=%lu)!",
+			  bp->header & ~0xf & 0xffff, 32);
 
-// 	// After realloc'ing x, we can return a block of size 32 to the freelist.
-// 	// This block will go into the freelist and be coalesced.
-// 	assert_free_block_count(0, 1);
-// 	assert_free_block_count(4016, 1);
-// }
+	// After realloc'ing x, we can return a block of size 32 to the freelist.
+	// This block will go into the freelist and be coalesced.
+	assert_free_block_count(0, 1);
+	assert_free_block_count(4016, 1);
+}
 
 // ############################################
 // STUDENT UNIT TESTS SHOULD BE WRITTEN BELOW
 // DO NOT DELETE THESE COMMENTS
 // ############################################
+Test(sfmm_student_suite, malloc_an_ld, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	int *x = sf_malloc(sizeof(long double));
 
-// Test(sfmm_student_suite, student_test_1, .timeout = TEST_TIMEOUT) {
-// }
+	cr_assert_not_null(x, "x is NULL!");
+
+	*x = 16;
+
+	cr_assert(*x == 16, "sf_malloc failed to give proper space for an long double!");
+
+	assert_free_block_count(0, 1);
+	assert_free_block_count(4016, 1);
+	assert_free_list_size(9, 1);
+
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+	cr_assert(sf_mem_start() + PAGE_SZ == sf_mem_end(), "Allocated more than necessary!");
+}
+Test(sfmm_student_suite, malloc_an_int_array, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	int *arr = sf_malloc(sizeof(int) * 10);
+	cr_assert_not_null(arr, "x is NULL!");
+
+	assert_free_block_count(0, 1);
+	assert_free_block_count(3984, 1);
+	assert_free_list_size(9, 1);
+
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+	cr_assert(sf_mem_start() + PAGE_SZ == sf_mem_end(), "Allocated more than necessary!");
+}
+Test(sfmm_student_suite, malloc_an_int_array_2, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	int *arr = sf_malloc(sizeof(int) * 100);
+	cr_assert_not_null(arr, "x is NULL!");
+
+	assert_free_block_count(0, 1);
+	assert_free_block_count(3632, 1);
+	assert_free_list_size(9, 1);
+
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+	cr_assert(sf_mem_start() + PAGE_SZ == sf_mem_end(), "Allocated more than necessary!");
+}
+
+Test(sfmm_student_suite, realloc_and_check, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	/* void *p = */ sf_malloc(16);
+	void *q = sf_malloc(150);
+	/* void *r = */ sf_malloc(5);
+	sf_free(q);
+
+	assert_free_block_count(0, 2);
+	assert_free_block_count(160, 0);
+	assert_free_block_count(3808, 1);
+	cr_assert(sf_errno == 0, "sf_errno is not zero!");
+}
+Test(sfmm_basecode_suite, malloc_ten_pages, .timeout = TEST_TIMEOUT)
+{
+	sf_errno = 0;
+	// We want to allocate up to exactly ten pages.
+	void *x = sf_malloc(40960 - 48 - (sizeof(sf_header) + sizeof(sf_footer)));
+
+	cr_assert_not_null(x, "x is NULL!");
+	assert_free_block_count(0, 0);
+	cr_assert(sf_errno == 0, "sf_errno is not 0!");
+}
